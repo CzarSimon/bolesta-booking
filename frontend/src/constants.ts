7bf7511e581cf14d4d0fc980a116b3e48a1621b8
Simon Lindgren <simon.g.lindgren@gmail.com>
2023-03-04 16:59:04 +0100

    frontend: WIP deployment

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -2,4 +2,4 @@ export const CLIENT_ID_KEY = "@bolesta-booking:frontend:CLIENT_ID";
 export const AUTH_TOKEN_KEY = "@bolesta-booking:frontend:AUTH_TOKEN";
 export const CURRENT_USER_KEY = "@bolesta-booking:frontend:CURRENT_USER_KEY";
 
-export const BASE_URL = "http://localhost:8080";
+export const BASE_URL = process.env.BASE_URL || "http://localhost:8080";
